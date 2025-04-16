b1ae8e18a6a703a9c31a4afd89a95011a3c33e9e
Yixuan Wu <y@ixuan.ch>
2025-04-16 12:17:17 +0000

    feat: Implement API endpoints for sports logs management

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) => {
   res.send('Good job Schnappi! Here is the SweatLog backend!');
 });
 
+app.use(express.json());
+
+let sportsLogs = [];
+
+app.post('/api/logs', (req, res) => {
+  const {sport, date} = req.body;
+  sportsLogs.push({sport, date});
+  res.json({success: true, logs: sportsLogs});
+})
+
+app.get('/api/logs', (req, res) => {
+  res.json({logs: sportsLogs});
+})
+
 app.listen(PORT, () => {
   console.log(`SweatLog backend is running on http:((localhost:${PORT} :3`);
 });
