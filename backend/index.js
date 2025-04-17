ec4a17bfeba1ba4736a361667e5bf46766ace72d
Yixuan Wu <y@ixuan.ch>
2025-04-17 08:37:42 +0000

    feat: Add sports logs management with JSON data file and update API for saving logs

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 4001;
+const fs = require('fs');
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.send('Good job Schnappi! Here is the SweatLog backend!');
@@ -8,11 +10,19 @@ app.get('/', (req, res) => {
 
 app.use(express.json());
 
-let sportsLogs = [];
+let sportsLogs;
+
+try {
+  sportsLogs = JSON.parse(fs.readFileSync(path.join(__dirname, '/data/sportslogs.json'), 'utf8'));
+} catch (error) {
+  console.error('Error reading sports logs:', error);
+  sportsLogs = [];
+}
 
 app.post('/api/logs', (req, res) => {
   const {sport, date} = req.body;
   sportsLogs.push({sport, date});
+  fs.writeFileSync(path.join(__dirname, '/data/sportslogs.json'), JSON.stringify(sportsLogs, null, 2));
   res.send(`Saved ${sport} on ${date}! Cool Schnappi!`);
 })
 
